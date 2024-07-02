1eecbd3b46b5ef135c11b89600eb708c3b2e06b0
Bogatov Dmitriy <darck538567@gmail.com>
2024-07-02 18:08:49 +0400

    Final 2

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
-    @Qualifier("timer")
+    @Qualifier("dog")
     private Animal animal;
     @Autowired
     private Timer timer;
@@ -17,7 +17,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
